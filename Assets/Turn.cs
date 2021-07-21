a539467f6d267dc074d8f3305fdd86d1d19c1792
HyoYongAnn <dksgydyd0617@naver.com>
2021-07-21 21:36:40 +0900

    sprites update

---
+++ Assets/Turn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour
+{
+    static private int moveChance = 0;
+    static public int MoveChance { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
